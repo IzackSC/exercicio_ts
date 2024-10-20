9ab8815b7af96cff520a9e7421ee936852130b98
IzackSC <colveroizack@gmail.com>
2024-10-19 22:05:53 -0300

    atualizando o exercício

--- src/atividade.js
+++ src/atividade.js
@@ -1,6 +1,13 @@
 "use strict";
+// Multiplicação //
 const numero1 = 5;
 const numero2 = 10;
 function multiplicaNumero(numero1, numero2) {
     return numero1 * numero2;
+}
+// Função de saudação //
+const nome = 'Izack';
+function saudacao(nome) {
+    return 'Olá' + nome;
+    // return `Olá ${nome}`
 }
